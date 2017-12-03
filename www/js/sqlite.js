05571506302180c1fb0d54df6393a7f1e2d43187
Arajane02 <30886014+Arajane02@users.noreply.github.com>
2017-12-03 17:12:16 +0800

    Update sqlite.js

--- www/js/sqlite.js
+++ www/js/sqlite.js
@@ -1,155 +1,264 @@
-"use strict";
-var storage = window.localStorage;
-var serverData = [
-  {
-  "todo": "Pick up milk from store"
-  }, {
-  "todo": "Make Dr. Appointment"
-  }, {
-  "todo": "Pay Water Bill"
-  }
-];
-var todoList = serverData.map(function (item) {
-  console.log(serverData)
-  console.log(item)
-  return item.todo;
-});
+'use strict';
 
-var Container = React.createClass({
-  displayName: "Container",
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-  getInitialState: function getInitialState() {
-    return {
-      list: this.props.toDoList
-    };
-  },
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
-  addToDoToList: function addToDoToList(newToDo) {
-    var newList = this.state.list;
-    newList.push(newToDo);
-    this.listUpdate(newList);
-  },
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-  removeToDo: function removeToDo(toDo) {
-    var newList = this.state.list;
-    newList = newList.filter(function (item) {
-      return item !== toDo;
-    });
-    this.listUpdate(newList);
-  },
+var todoItems = [{
+    name: 'Kiss Argel',
+    completed: true
+}, {
+    name: 'Pass the Exam',
+    completed: false
+}, {
+    name: 'Present to sir Rufo',
+    completed: false
+}];
 
-  listUpdate: function listUpdate(newList) {
-    //console.log('updating list', newList);
-    storage.setItem("todoList", JSON.stringify(newList));
-    this.setState({
-      list: newList
-    });
-  },
+var App = function (_React$Component) {
+    _inherits(App, _React$Component);
 
-  render: function render() {
-    var count = this.state.list.length;
-    return React.createElement(
-      "div",
-      { className: "main-container" },
-      React.createElement(
-        "div",
-        { className: "header-div" },
-        "Dailiry App"
-      ),
-      React.createElement(AddToDoBox, { addItemToList: this.addToDoToList }),
-      React.createElement(ToDoItems, { toDoList: this.state.list, removeToDo: this.removeToDo }),
-      React.createElement(ToDoCount, { count: count })
-    );
-  }
-});
+    function App(props) {
+        _classCallCheck(this, App);
 
-var AddToDoBox = React.createClass({
-  displayName: "AddTextBox",
+        var _this = _possibleConstructorReturn(this, _React$Component.call(this, props));
 
-  addToDo: function addToDo(e) {
-    e.preventDefault();
-    var newToDo = this.refs.AddedItem.value;
-    if (newToDo === "") {
-      return;
+        _this.state = { todoItems: todoItems };
+        return _this;
     }
-    this.refs.AddedItem.value = "";
-    this.props.addItemToList(newToDo);
-  },
-  render: function render() {
-    return React.createElement(
-      "div",
-      { className: "input-div" },
-      React.createElement(
-        "form",
-        null,
-        React.createElement("input", { type: "text", placeholder: " Start typing...", ref: "AddedItem", id: "add-item-input" }),
-        React.createElement("input", { type: "submit", id: "submitToDo", value: "Add", onClick: this.addToDo, id: "add-item-submit" })
-      )
-    );
-  }
-});
 
-var ToDoItems = React.createClass({
-  displayName: "DailyItems",
+    App.prototype.render = function render() {
+        var _this2 = this;
 
-  removeItem: function remove(e) {
-    var toDo = e.target.dataset.item;
-    this.props.removeToDo(toDo);
-  },
+        var items = this.state.todoItems;
+        items = items.map(function (item, index) {
+            return React.createElement(TodoItem, { item: item, key: index, onDelete: _this2.onDelete.bind(_this2), onSave: _this2.onSave.bind(_this2), toggleComplete: _this2.toggleComplete.bind(_this2) });
+        });
+        return React.createElement(
+            'div',
+            null,
+            React.createElement(
+                'h1',
+                { className: 'header' },
+                ' My to do List '
+            ),
+            React.createElement(AddItem, { onAdd: this.onAdd.bind(this), items: this.state.todoItems }),
+            React.createElement(
+                'ul',
+                { className: 'todo-list' },
+                items
+            )
+        );
+    };
 
-  render: function render() {
-    var items = this.props.toDoList.map(function (item) {
-      return React.createElement(
-        "div",
-        { className: "todo-items" },
-        React.createElement(
-          "span",
-          { className: "item-text" },
-          item
-        ),
-        React.createElement("div", { className: "item-clickable", onDoubleClick: this.removeItem, "data-item": item })
-      );
-    }.bind(this));
-    return React.createElement(
-      "div",
-      null,
-      items
-    );
-  }
-});
+    App.prototype.onDelete = function onDelete(item) {
+        var updatedItems = this.state.todoItems;
+        updatedItems = updatedItems.filter(function (value, index) {
+            return item !== value;
+        });
+        this.setState({
+            todoItems: updatedItems
+        });
+    };
 
-var ToDoCount = React.createClass({
-  displayName: "ItemsCount",
+    App.prototype.onAdd = function onAdd(newTaskName) {
+        var updatedItems = this.state.todoItems;
+        updatedItems.push({
+            name: newTaskName,
+            completed: false
+        });
+        this.setState({
+            todoItems: updatedItems
+        });
+    };
 
-  render: function render() {
-    return React.createElement(
-      "div",
-      { className: "count-div" },
-      React.createElement(
-        "span",
-        null,
-        "Daily Items Left : "
-      ),
-      React.createElement(
-        "div",
-        { className: "count-display" },
-        React.createElement(
-          "span",
-          { className: "count-text" },
-          this.props.count
-        )
-      )
-    );
-  }
-});
+    App.prototype.onSave = function onSave(oldItem, newName) {
+        var thisItem = this.state.todoItems.filter(function (item) {
+            return item === oldItem;
+        })[0];
+        thisItem.name = newName;
+        this.setState({
+            todoItems: this.state.todoItems
+        });
+    };
 
+    App.prototype.toggleComplete = function toggleComplete(clickedItem) {
+        var thisItem = this.state.todoItems.filter(function (item) {
+            return item === clickedItem;
+        })[0];
+        thisItem.completed = !thisItem.completed;
+        this.setState({
+            todoItems: this.state.todoItems
+        });
+    };
 
+    return App;
+}(React.Component);
 
-if(storage.getItem("todoList") === null){
-  storage.setItem("todoList", JSON.stringify(todoList));
-} else {
-  todoList = JSON.parse(storage.getItem("todoList"));
-};
+var AddItem = function (_React$Component2) {
+    _inherits(AddItem, _React$Component2);
 
+    function AddItem(props) {
+        _classCallCheck(this, AddItem);
 
-ReactDOM.render(React.createElement(Container, { toDoList: todoList }), document.getElementById("root"));
+        var _this3 = _possibleConstructorReturn(this, _React$Component2.call(this, props));
+
+        _this3.state = {
+            infoMessage: ''
+        };
+        return _this3;
+    }
+
+    AddItem.prototype.render = function render() {
+        return React.createElement(
+            'form',
+            { className: 'add-item-form', onSubmit: this.handleSubmit.bind(this) },
+            React.createElement('input', { className: 'add-item-input', type: 'text', placeholder: 'a new task to do...', ref: 'newItem' }),
+            React.createElement('input', { className: 'add-item-button', type: 'submit', value: 'add' }),
+            React.createElement(
+                'p',
+                { className: 'add-item-info' },
+                ' ',
+                this.state.infoMessage,
+                ' '
+            )
+        );
+    };
+
+    AddItem.prototype.handleSubmit = function handleSubmit(e) {
+        e.preventDefault();
+        var value = this.refs.newItem.value;
+        var isInList = this.props.items.filter(function (item) {
+            return item.name.toUpperCase() == value.toUpperCase();
+        }).length;
+        console.log(isInList);
+
+        if (!value) {
+            this.setState({
+                infoMessage: 'you want to add an empty task?'
+            });
+        } else if (isInList) {
+            this.setState({
+                infoMessage: 'this task already exists!'
+            });
+        } else {
+            this.props.onAdd(value);
+            this.refs.newItem.value = '';
+            this.setState({
+                infoMessage: ''
+            });
+        }
+    };
+
+    return AddItem;
+}(React.Component);
+
+var TodoItem = function (_React$Component3) {
+    _inherits(TodoItem, _React$Component3);
+
+    function TodoItem(props) {
+        _classCallCheck(this, TodoItem);
+
+        var _this4 = _possibleConstructorReturn(this, _React$Component3.call(this, props));
+
+        _this4.state = {
+            editing: false
+        };
+        return _this4;
+    }
+
+    TodoItem.prototype.render = function render() {
+        return React.createElement(
+            'li',
+            { className: 'todo-item' },
+            this.renderTodoItem()
+        );
+    };
+
+    TodoItem.prototype.renderTodoItem = function renderTodoItem() {
+        var isCompleted = this.props.item.completed;
+
+        if (this.state.editing) {
+            return React.createElement(
+                'form',
+                { className: 'todo-item-wrapper', onSubmit: this.handleSave.bind(this) },
+                React.createElement('input', { className: 'editing-form-input', type: 'text', ref: 'editingItem', defaultValue: this.props.item.name, onFocus: this.handleFocus.bind(this), autoFocus: true }),
+                this.renderButtons()
+            );
+        }
+
+        return React.createElement(
+            'div',
+            { className: 'todo-item-wrapper' },
+            React.createElement(
+                'p',
+                { className: isCompleted ? 'todo-item-name--completed' : 'todo-item-name', onClick: this.props.toggleComplete.bind(this, this.props.item) },
+                this.props.item.name
+            ),
+            this.renderButtons()
+        );
+    };
+
+    TodoItem.prototype.renderButtons = function renderButtons() {
+        if (this.state.editing) {
+            return React.createElement(
+                'div',
+                null,
+                React.createElement(
+                    'button',
+                    { className: 'button', type: 'button', onClick: this.handleSave.bind(this) },
+                    ' save '
+                ),
+                React.createElement(
+                    'button',
+                    { className: 'button', type: 'button', onClick: this.onCancel.bind(this) },
+                    ' cancel '
+                )
+            );
+        }
+        return React.createElement(
+            'div',
+            null,
+            React.createElement(
+                'button',
+                { className: 'button', type: 'button', onClick: this.onEdit.bind(this) },
+                ' edit '
+            ),
+            React.createElement(
+                'button',
+                { className: 'button', type: 'button', onClick: this.props.onDelete.bind(this, this.props.item) },
+                ' delete '
+            )
+        );
+    };
+
+    TodoItem.prototype.onEdit = function onEdit() {
+        this.setState({
+            editing: true
+        });
+    };
+
+    TodoItem.prototype.onCancel = function onCancel() {
+        this.setState({
+            editing: false
+        });
+    };
+
+    TodoItem.prototype.handleSave = function handleSave(e) {
+        e.preventDefault();
+        this.setState({
+            editing: false
+        });
+        this.props.onSave(this.props.item, this.refs.editingItem.value);
+    };
+
+    TodoItem.prototype.handleFocus = function handleFocus(e) {
+        e.target.select();
+    };
+
+    return TodoItem;
+}(React.Component);
+
+ReactDOM.render(React.createElement(App, null), document.getElementById('app'));
